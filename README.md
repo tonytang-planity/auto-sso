a440e2910a3c36a631d11479986a9b27bbbe1c80
Victor-Dufayet-Planity <victor.dufayet@planity.com>
2024-03-25 16:33:34 +0100

    ✨ Make works the second button click & update readme

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ We need a way to make it faster.
 Make a chrome extension to validate the form automatically for us.
 
 1. clone the repository
-2. Replace `TODO: paste sso URL here` inside the [manifest.json](manifest.json) file by your sso URL and add `/*` at the end. If the url is `https://myssourl`, you should have `https://myssourl/*`.
-3. Replace `TODO: paste device check URL here` inside the [manifest.json](manifest.json) file by your device check URL and add `/*` at the end. If the url is `https://mydeviceurl`, you should have `https://mydeviceurl/*`.
+2. Replace `SSO_URL` inside the [manifest.json](manifest.json) & the [auto-sso.js](scripts/auto-sso.js) files by your sso URL (without last slash `/`). Ex: `https://device.sso.xx-xxxx-x.amazonaws.com`
+3. Replace `DEVICE_URL` inside the [manifest.json](manifest.json) & the [auto-sso.js](scripts/auto-sso.js) files by your device check URL (without last slash `/`). Ex: `https://x-xxxxxxxxxx.awsapps.com/start`
 4. Follow the instructions to create an extension (until step 2) : https://support.google.com/chrome/a/answer/2714278?hl=en
--- scripts/auto-sso.js
+++ scripts/auto-sso.js
@@ -1,5 +1,19 @@
-const allowBtn = document.getElementById("cli_login_button") ?? document.getElementById("cli_verification_btn");
+// Clicking the verification button on the first page
+if (window.location.href.startsWith("SSO_URL")) {
+  const confirmBtn = document.getElementById("cli_verification_btn")
+  confirmBtn?.click()
+}
 
-if (!!allowBtn) {
-  allowBtn.click();
+// Observing mutations on the second page
+if (window.location.href.startsWith("DEVICE_URL")) {
+  const observer = new MutationObserver((mutationsList, observer) => {        // Creating a MutationObserver to watch for changes in the DOM
+    mutationsList.forEach(mutation => {                                       // Looping through each mutation in the mutations list
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {  // Checking if the mutation type is 'childList' and if nodes were added to the DOM
+        const allowBtn = document.getElementById("cli_login_button")
+        allowBtn?.click()
+      }
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
 }
